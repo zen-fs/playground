a28af7fe5159c80f8a9ce144caac0f888542228c
James Prevett <jp@jamespre.dev>
2024-10-13 16:08:52 -0500

    Added file size formatting

--- src/explorer.ts
+++ src/explorer.ts
@@ -1,17 +1,20 @@
 import { fs } from '@zenfs/core';
 import { cwd, join } from '@zenfs/core/emulation/path.js';
 import $ from 'jquery';
+import { formatCompact } from 'utilium';
 import { cloneTemplate } from 'utilium/dom.js';
+
+const endsWithLetter = /[^\d]$/;
 
 function createEntry(name: string) {
 	const stats = fs.statSync(join(cwd, name));
 
 	const li = $(cloneTemplate('#entry')).find('li');
 
+	const size = formatCompact(stats.size);
 	li.find('.name').text(name);
-	li.find('.size').text(stats.size);
+	li.find('.size').text(size + (endsWithLetter.test(size) ? 'B' : ' bytes'));
 	li.find('.mtime').text(stats.mtime.toLocaleString());
-
 	li.appendTo('#explorer');
 }
 
